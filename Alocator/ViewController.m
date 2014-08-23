1007cab51a6a9d050ba7a24689a94f982f5da58b
Zach Shallbetter <zachshallbetter@gmail.com>
2014-08-23 11:50:36 -0700

    Initial Commit

---
+++ Alocator/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Alocator
+//
+//  Created by Zach Shallbetter on 8/23/14.
+//  Copyright (c) 2014 Bloc. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+            
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ Alocator/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Alocator
+//
+//  Created by Zach Shallbetter on 8/23/14.
+//  Copyright (c) 2014 Bloc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
